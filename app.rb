830bf2ae91450dc698cf6db6b29b23a2f5ffcb8c
Tom Damant <tom@gluon.local>
2019-01-21 16:53:39 +0000

    complete post params steps

--- app.rb
+++ app.rb
@@ -17,10 +17,20 @@ get '/sample' do
   "sample"
 end
 
-get '/named-cat' do
+# get '/named-cat' do
+#   p params
+#   @name = params[:name]
+#   erb :cat_form
+# end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
 
 get '/random-cat' do
